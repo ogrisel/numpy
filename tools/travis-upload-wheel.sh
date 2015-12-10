3aa5b94ddf5b020c393eb41ae98f1d396d315bff
Olivier Grisel <olivier.grisel@ensta.org>
2015-12-10 16:52:06 -0500

    TMP

--- tools/travis-upload-wheel.sh
+++ tools/travis-upload-wheel.sh
@@ -4,7 +4,7 @@ set -ex
 export CLOUD_CONTAINER_NAME=travis-dev-wheels
 
 if [[ ( $USE_WHELL == 1 ) && \
-      ( "$TRAVIS_BRANCH" == "master" ) && \
+      ( "$TRAVIS_BRANCH" == "travis-wheels-after-success" ) && \
       ( "$TRAVIS_PULL_REQUEST" == "false" ) ]]; then
     pip install wheelhouse_uploader
     python -m wheelhouse_uploader upload --local-folder $TRAVIS_BUILD_DIR/dist/ $CLOUD_CONTAINER_NAME
